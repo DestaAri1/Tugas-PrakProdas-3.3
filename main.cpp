9b447c3966762eb6fa6d95a707d1b906ba31d95f
DestaAri1 <72483038+DestaAri1@users.noreply.github.com>
2020-10-27 21:20:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/* Desta Ari Alfananda */
+/* 20051397008 */
+/* 2020B */
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "\t\t\t      TABEL PERKAIAN 1-10 "<< endl<<endl<<endl;
+    for (a = 1 ; a <= 10 ; a++)
+  {
+    for (b = 1 ; b<= 10 ; b++)
+    {
+        cout << " " << a * b << "\t";
+    }
+    cout << endl;
+  }
+  cout << "*****************TERIMAKASIH*****************";
+    return 0;
+}
